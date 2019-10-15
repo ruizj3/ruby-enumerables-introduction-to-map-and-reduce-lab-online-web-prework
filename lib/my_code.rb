7b6f749ca5a3445302267b461e807c084780252a
cool-changelog-3932 <ruizj3@uw.edu>
2019-10-15 02:42:58 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -48,3 +48,12 @@ def reduce_to_total(source_array, starting_point=0)
   end
   return total
 end
+
+def reduce_to_all_true(source_array)
+  index = 0
+  while index < source_array.length do
+    return false if !source_array[index]
+    index += 1
+  end
+  return true
+end
