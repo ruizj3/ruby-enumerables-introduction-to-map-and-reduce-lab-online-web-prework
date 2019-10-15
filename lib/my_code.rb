f91737647ef11c0a14dab2b30d7c76ff74037af4
cool-changelog-3932 <ruizj3@uw.edu>
2019-10-15 02:41:08 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -38,3 +38,13 @@ def map_to_square(source_array)
   end
   return new_array
 end
+
+def reduce_to_total(source_array, starting_point=0)
+  total = starting_point
+  index = 0
+  while index < source_array.length do
+    total += source_array[index]
+    index += 1
+  end
+  return total
+end
