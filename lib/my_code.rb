f2bf9552ccca1ab2e177395b947b68c76dd123c6
cool-changelog-3932 <ruizj3@uw.edu>
2019-10-15 02:34:47 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,10 @@
 # My Code here....
+def map_to_negativize(source_array)
+  new_array = []
+  index = 0
+  while index < source_array.length do
+    new_array.push( source_array[index] * -1 )
+    index += 1
+  end
+  return new_array
+end
